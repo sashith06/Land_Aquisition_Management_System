fefee73a4c8345f27a59cc92c1ddadf6c049a670
Sandeepa Hiripitiya <umeshsandeepa1@gmail.com>
2025-10-27 00:48:15 +0530

    Add CORS debug logging2

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174'
+  'http://127.0.0.1:5174',
+  'https://land-aquisition-management-system.vercel.app'
 ];
 
 // Add production frontend URL if specified
